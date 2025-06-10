cbecb9e800416a328d54188d09ccf09725178ae8
dorkem <20233144@gs.cwnu.ac.kr>
2025-06-10 17:57:10 +0900

    memo: 초반세팅

---
+++ backend/src/main/java/com/team3/airdnd/AirdndApplication.java
@@ -0,0 +1,13 @@
+package com.team3.airdnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdndApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/team3/airdnd/AirdndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.airdnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirdndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
