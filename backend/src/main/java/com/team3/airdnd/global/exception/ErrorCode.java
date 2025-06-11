cf1ff31a594ab9c875a3c34a948cee167adc2508
dorkem <20233144@gs.cwnu.ac.kr>
2025-06-11 12:14:25 +0900

    feat: exception 및 공통 ResponseDTO 생성

---
+++ backend/src/main/java/com/team3/airdnd/global/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.team3.airdnd.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+  NOT_FOUND_RESOURCE(40400, HttpStatus.NOT_FOUND, "해당 리소스가 존재하지 않습니다."),
+  NOT_FOUND_USER(40401, HttpStatus.NOT_FOUND, "해당 사용자가 존재하지 않습니다."),
+
+  FAILURE_LOGIN(40100, HttpStatus.UNAUTHORIZED, "잘못된 아이디 또는 비밀번호입니다."),
+  ACCESS_DENIED(40300, HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+
+  INTERNAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러입니다.");
+
+  private final Integer code;
+  private final HttpStatus httpStatus;
+  private final String message;
+}
