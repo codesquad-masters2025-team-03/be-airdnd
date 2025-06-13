0dbf0297e2b97cc564d3ed3cf5031a02836d9046
Gyuwon Moon <gyuwon822@gmail.com>
2025-06-13 17:13:23 +0900

    test: 테스트 컨테이너 적용

--- backend/src/test/java/com/team3/airdnd/auth/repository/UserRepositoryTest.java
+++ backend/src/test/java/com/team3/airdnd/auth/repository/UserRepositoryTest.java
@@ -13,22 +13,19 @@ import com.team3.airdnd.user.repository.UserRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-@DataJpaTest
-public class UserRepositoryTest {
-  @Container
-  static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.42")
-      .withDatabaseName("testdb")
-      .withUsername("testuser")
-      .withPassword("testpass");
+import com.team3.airdnd.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.team3.airdnd.user.domain.User;
+import com.team3.airdnd.user.repository.UserRepository;
 
-  @DynamicPropertySource
-  static void setDatasourceProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
-    registry.add("spring.datasource.username", mysqlContainer::getUsername);
-    registry.add("spring.datasource.password", mysqlContainer::getPassword);
-    registry.add("spring.datasource.driver-class-name", mysqlContainer::getDriverClassName);
-  }
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest extends AbstractIntegrationTest {
 
   @Autowired
   private UserRepository userRepository;
@@ -36,18 +33,17 @@ public class UserRepositoryTest {
   @Test
   void 회원가입_테스트() {
     User user = User.builder()
-        .loginId("123")
-        .password("123")
-        .email("test@example.com")
-        .username("bazzi")
-        .phone("01012341234")
-        .role(User.Role.GUEST)
-        .build();
+            .loginId("123")
+            .password("123")
+            .email("test@example.com")
+            .username("bazzi")
+            .phone("01012341234")
+            .role(User.Role.GUEST)
+            .build();
 
     userRepository.save(user);
 
     User saved = userRepository.findByLoginId("123").orElseThrow();
     assertThat(saved.getUsername()).isEqualTo("bazzi");
   }
-
 }
