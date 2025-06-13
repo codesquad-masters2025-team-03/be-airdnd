889d386b74cceb6dbea1b219340b869dfa234d5f
Gyuwon Moon <gyuwon822@gmail.com>
2025-06-13 18:01:56 +0900

    feat: 숙소 상세 조회 API 작성

---
+++ backend/src/main/java/com/team3/airdnd/accommodation/dto/AmenityInfoDto.java
@@ -0,0 +1,6 @@
+package com.team3.airdnd.accommodation.dto;
+
+public record AmenityInfoDto(
+        Long id,
+        String name
+) {}
---
+++ backend/src/main/java/com/team3/airdnd/storedFile/dto/ImageUrlDto.java
@@ -0,0 +1,6 @@
+package com.team3.airdnd.storedFile.dto;
+
+public record ImageUrlDto(
+        Long id,
+        String imageUrl
+) {}
