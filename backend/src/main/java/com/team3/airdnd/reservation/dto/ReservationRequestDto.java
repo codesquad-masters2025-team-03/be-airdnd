b5907c2207dba98b37fc30618b05dc5258a36b99
Gyuwon Moon <gyuwon822@gmail.com>
2025-06-18 19:20:53 +0900

    feat: Reservation Dto 추가

---
+++ backend/src/main/java/com/team3/airdnd/reservation/dto/ReservationRequestDto.java
@@ -0,0 +1,21 @@
+package com.team3.airdnd.reservation.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class ReservationRequestDto {
+
+	@Getter
+	@Builder
+	@AllArgsConstructor
+	public static class CreateReservationRequestDto {
+		private Long accommodationId;
+		private LocalDate checkIn;
+		private LocalDate checkOut;
+		private int guestCount;
+		private Long userId;
+	}
+}
---
+++ backend/src/main/java/com/team3/airdnd/reservation/dto/ReservationResponseDto.java
@@ -0,0 +1,19 @@
+package com.team3.airdnd.reservation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class ReservationResponseDto {
+
+	@Getter
+	@Builder
+	@AllArgsConstructor
+	public static class CreateReservationResponseDto {
+		private Long reservationId;
+		private String orderId;
+		private String status;
+		private int amount;
+	}
+
+}
