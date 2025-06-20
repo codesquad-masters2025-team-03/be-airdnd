f24b2b4e58def8c085db031fd051a5a712e59dff
Gyuwon Moon <gyuwon822@gmail.com>
2025-06-20 19:20:36 +0900

    refactor: S3 자격 인증 방식 전환 *DefaultCredentialsProvider

--- backend/src/main/java/com/team3/airdnd/aws/s3Config.java
+++ backend/src/main/java/com/team3/airdnd/aws/s3Config.java
@@ -4,19 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class s3Config {
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
-
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String secretKey;
-
 	@Value("${cloud.aws.region.static}")
 	private String region;
 
@@ -24,9 +17,7 @@ public class s3Config {
 	public S3Client s3Client() {
 		return S3Client.builder()
 			.region(Region.of(region))
-			.credentialsProvider(
-				StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
-			)
+			.credentialsProvider(DefaultCredentialsProvider.create())
 			.build();
 	}
 }
