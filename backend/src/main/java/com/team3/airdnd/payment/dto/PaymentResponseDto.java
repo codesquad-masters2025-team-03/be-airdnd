3271dd5f412ae52c0582b587c7e63ab55c0fdf58
jkchoi490 <jkchoi490@daum.net>
2025-06-22 19:38:35 +0900

    feat: 결제 기능 RequestDto, ResponseDto 개발

---
+++ backend/src/main/java/com/team3/airdnd/payment/dto/PaymentResponseDto.java
@@ -0,0 +1,23 @@
+package com.team3.airdnd.payment.dto;
+
+import java.time.LocalDateTime;
+
+import com.team3.airdnd.payment.domain.Payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponseDto {
+	private Long id;
+	private Long reservationId;
+	private Long paymentMethodId;
+	private String orderId;
+	private Long amount;
+	private LocalDateTime paidAt;
+}
