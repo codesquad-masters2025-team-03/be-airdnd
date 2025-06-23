54fdaaab86ff9d5c6247be4f396a22d962e3530a
jkchoi490 <jkchoi490@daum.net>
2025-06-23 12:24:54 +0900

    feat: 결제 단건 조회 기능 개발(Controller)

--- backend/src/main/java/com/team3/airdnd/payment/controller/PaymentController.java
+++ backend/src/main/java/com/team3/airdnd/payment/controller/PaymentController.java
@@ -1,9 +1,12 @@
 package com.team3.airdnd.payment.controller;
 
+import java.util.List;
+
 import com.team3.airdnd.payment.domain.Payment;
 import com.team3.airdnd.payment.dto.PaymentRequestDto;
 import com.team3.airdnd.payment.dto.PaymentResponseDto;
 import com.team3.airdnd.payment.service.PaymentService;
+import com.team3.airdnd.reservation.domain.Reservation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,31 @@ public class PaymentController {
 	@PostMapping("/approve")
 	public ResponseEntity<PaymentResponseDto> approvePayment(@RequestBody PaymentRequestDto paymentRequestDto) {
 		Payment payment = paymentService.approvePayment(paymentRequestDto);
-		return ResponseEntity.ok(paymentService.toDto(payment));
+		PaymentResponseDto responseDto = PaymentResponseDto.toDto(payment);
+		return ResponseEntity.ok(responseDto);
 	}
+
+	//paymentId로 조회
+	@GetMapping("/{paymentId}")
+	public ResponseEntity<PaymentResponseDto> getPaymentById(@PathVariable Long paymentId) {
+		PaymentResponseDto responseDto = paymentService.getPaymentById(paymentId);
+		return ResponseEntity.ok(responseDto);
+	}
+
+	//guestId & status 로 조회
+	@GetMapping("/by-guest")
+	public ResponseEntity<List<PaymentResponseDto>> getPaymentsByGuest(
+		@RequestParam Long guestId,
+		@RequestParam(required = false) Reservation.Status status
+	) {
+		List<Payment> payments = paymentService.getPaymentsByGuestId(guestId, status);
+		List<PaymentResponseDto> response = payments.stream()
+			.map(PaymentResponseDto::toDto)
+			.toList();
+		return ResponseEntity.ok(response);
+	}
+
+
+
+
 }
