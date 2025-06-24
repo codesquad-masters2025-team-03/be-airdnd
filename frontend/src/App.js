f5ace9f1aa3b62f28aa5e37b3b93f821d7dd60f6
dorkem <20233144@gs.cwnu.ac.kr>
2025-06-24 16:30:07 +0900

    feat: (임시) 채팅방 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import ChatRoom from './components/ChatRoom';
 import AccommodationListPage from './pages/AccommodationListPage';
 import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/accommodations" element={<AccommodationListPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<MainPage/>}/>
+                    <Route path="/accommodations" element={<AccommodationListPage/>}/>
+                    <Route path="/chatroom" element={<ChatRoom/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
