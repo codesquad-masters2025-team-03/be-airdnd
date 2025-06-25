349c0351b47ea51d8e4469a9b321cf2c0db96f95
jkchoi490 <jkchoi490@daum.net>
2025-06-25 12:05:45 +0900

    feat : 호스트, 게스트 예약 조회 Dto 개발

--- backend/src/main/java/com/team3/airdnd/reservation/dto/HostReservationDto.java
+++ backend/src/main/java/com/team3/airdnd/reservation/dto/HostReservationDto.java
@@ -11,31 +11,34 @@ import lombok.Getter;
 
 @Builder
 @Getter
-public class GuestReservationDto {
+public class HostReservationDto {
 	private Long reservationId;
+	private String guestName;
 	private String orderId;
 	private String accommodationName;
 	private String coverImageUrl;
 	private LocalDate checkIn;
 	private LocalDate checkOut;
-	private Long totalPrice;
-	private Long serviceFee;
-	private LocalDateTime createdAt;
 	private Reservation.Status status;
+	private Long serviceFee;
+	private Long totalPrice;
+	private LocalDateTime createdAt;
 
 	@QueryProjection
-	public GuestReservationDto(Long reservationId, String orderId, String accommodationName, String coverImageUrl,
-		LocalDate checkIn, LocalDate checkOut,
-		Long totalPrice, Long serviceFee, LocalDateTime createdAt, Reservation.Status status) {
+	public HostReservationDto(Long reservationId, String guestName, String orderId, String accommodationName,
+		String coverImageUrl,
+		LocalDate checkIn, LocalDate checkOut, Reservation.Status status,
+		Long serviceFee, Long totalPrice, LocalDateTime createdAt) {
 		this.reservationId = reservationId;
+		this.guestName = guestName;
 		this.orderId = orderId;
 		this.accommodationName = accommodationName;
 		this.coverImageUrl = coverImageUrl;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		this.totalPrice = totalPrice;
-		this.serviceFee = serviceFee;
-		this.createdAt = createdAt;
 		this.status = status;
+		this.serviceFee = serviceFee;
+		this.totalPrice = totalPrice;
+		this.createdAt = createdAt;
 	}
 }
