97b7f47436db7d3b8f0e88530ba0cd14397a3b2b
Gyuwon Moon <gyuwon822@gmail.com>
2025-06-26 12:46:28 +0900

    refactor: 인터셉터 제외 경로 추가

--- backend/src/main/java/com/team3/airdnd/global/config/WebConfig.java
+++ backend/src/main/java/com/team3/airdnd/global/config/WebConfig.java
@@ -33,7 +33,10 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(authInterceptor)
 			.addPathPatterns("/api/**")
-			.excludePathPatterns("/api/auth/**");
+			.excludePathPatterns("/api/auth/**",
+				"api/accommodations",
+				"api/accommodations/{accommodationId}",
+				"api/accommodations//price-range");
 	}
 
 	@Bean
