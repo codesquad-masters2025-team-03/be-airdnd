238e77a5089c8a5fbfd4754fffa1ddc2e7747fc4
Gyuwon Moon <gyuwon822@gmail.com>
2025-06-26 16:14:25 +0900

    refactor: 인터셉터 수정

--- backend/src/main/java/com/team3/airdnd/auth/AuthInterceptor.java
+++ backend/src/main/java/com/team3/airdnd/auth/AuthInterceptor.java
@@ -17,6 +17,10 @@ public class AuthInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
+		
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			return true;
+		}
 
 		String token = extractToken(request);
 
